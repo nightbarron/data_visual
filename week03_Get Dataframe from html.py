f2bd309f1fe191b038b197b12a256e0483afe5a2
nightbarron <hoangtuan140982@gmail.com>
2021-02-17 07:54:26 +0700

    v 1.3 get location

--- week03_Get Dataframe from html.py
+++ week03_Get Dataframe from html.py
@@ -4,6 +4,8 @@ import ssl
 
 import pandas as pd
 
+#Part 1
+
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
@@ -15,6 +17,34 @@ soup = BeautifulSoup(html, 'html.parser')
 tags = soup('table')
 
 df = pd.read_html(html)[0]
+df = df[df["Neighbourhood"] != 'Not assigned'] #Filter
 print(df)
 
+
+# Part 2
+import geocoder # import geocoder
+
+# initialize your variable to None
+lat_lng_coords = None
+latitude = list()
+longitude = list()
+noneCount = 0
+# loop until you get the coordinates
+for postal_code in df["Postal Code"]:
+    while (lat_lng_coords is None):
+        g = geocoder.google('{}, Toronto, Ontario'.format(postal_code))
+        lat_lng_coords = g.latlng
+        noneCount = noneCount + 1
+        if noneCount == 10:
+            noneCount = 0
+            lat_lng_coords = [1,1]
+
+    print(lat_lng_coords)
+    latitude.append(lat_lng_coords[0])
+    longitude.append(lat_lng_coords[1])
+
+df["Latitude"] = latitude
+df["Longitude"] = longitude
+
+print(df)
 	
